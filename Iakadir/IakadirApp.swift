c9c9aa8f294189cd4dd41d35a615921a0e552f0a
Ilan <ilan.outyey@gmail.com>
2024-11-28 14:03:05 +0100

    ✨ Init

---
+++ Iakadir/IakadirApp.swift
@@ -0,0 +1,17 @@
+//
+//  IakadirApp.swift
+//  Iakadir
+//
+//  Created by digital on 28/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct IakadirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IakadirTests/IakadirTests.swift
@@ -0,0 +1,17 @@
+//
+//  IakadirTests.swift
+//  IakadirTests
+//
+//  Created by digital on 28/11/2024.
+//
+
+import Testing
+@testable import Iakadir
+
+struct IakadirTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
